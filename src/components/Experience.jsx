cee8fbc8a6b80a0a3b00cf490b53d1211c90137a
mrs.stjuste <brianstjuste2021@gmail.com>
2024-10-22 18:37:47 -0400

    commit rq

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,7 @@ import { styles } from "../styles";
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
-
+// blah
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
